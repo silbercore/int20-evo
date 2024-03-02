b30f03006c35f0fb6aced1437f87db6adb1a772b
C-22-18 <pustovoyt.schl@gmail.com>
2024-03-02 23:22:17 +0200

    ass

--- src/pages/router.py
+++ src/pages/router.py
@@ -15,9 +15,9 @@ templates = Jinja2Templates(directory="pages/templates")
 def get_base_page(request: Request, user: User = Depends(current_user)):
     return templates.TemplateResponse("index.html", {"request": request, "user":user})
 
-@router.get("/reg")
+@router.get("/login")
 def get_base_page(request: Request):
-    return templates.TemplateResponse("reg.html", {"request": request})
+    return templates.TemplateResponse("login.html", {"request": request})
 
 @router.get("/tests")
 def get_base_page(request: Request):
@@ -26,10 +26,6 @@ def get_base_page(request: Request):
 @router.get("/raitings")
 def get_base_page(request: Request):
     return templates.TemplateResponse("raitings.html", {"request": request})
-
-@router.get("/login")
-def get_base_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
 
 @router.get("/students")
 def get_base_page(request: Request):
